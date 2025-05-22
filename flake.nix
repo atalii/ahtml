f53a0918c40b79f757f755aa7cfe770c5481c4fb
Tali Auster <me@tali.network>
2025-05-22 11:32:46 -0600

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      systems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      define = f: forAllSystems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = define (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            alire
+            gnat14
+            gnat14Packages.gprbuild
+          ];
+        };
+      });
+    };
+}
